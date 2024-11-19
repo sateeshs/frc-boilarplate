aa722b46e8a6068167cd3d2c7ed1b49e9435bb07
Sateesh Sabbineni <sateesh.sabbineni@hotmail.com>
2024-11-19 16:38:39 -0500

    LED example added

---
+++ addressable-led/src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -0,0 +1,12 @@
+// package frc.robot.subsystem;
+
+// import edu.wpi.first.wpilibj.AddressableLED;
+// import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// public class LedSubsystem extends SubsystemBase {
+// private final AddressableLED m_led;
+//   private final AddressableLEDBuffer m_ledData;
+//   private final AddressableLEDBufferView m_leftSide;
+//   private final AddressableLEDBufferView m_rightSide;    
+// }
